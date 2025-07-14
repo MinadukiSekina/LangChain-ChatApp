80aaaebb37788b331d3d0e17a0a9abced5c4a46a
MinadukiSekina <taroufujimoto075@gmail.com>
2025-07-14 01:42:00 +0000

    feat: add main chat application logic

---
+++ src/chat_app/main.py
@@ -0,0 +1,38 @@
+import getpass
+import os
+
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage, SystemMessage
+
+try:
+    # load environment variables from .env file (requires `python-dotenv`)
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except ImportError:
+    pass
+
+os.environ["LANGSMITH_TRACING"] = "true"
+if "LANGSMITH_API_KEY" not in os.environ:
+    os.environ["LANGSMITH_API_KEY"] = getpass.getpass(
+        prompt="Enter your LangSmith API key (optional): "
+    )
+if "LANGSMITH_PROJECT" not in os.environ:
+    os.environ["LANGSMITH_PROJECT"] = getpass.getpass(
+        prompt='Enter your LangSmith Project Name (default = "default"): '
+    )
+    if not os.environ.get("LANGSMITH_PROJECT"):
+        os.environ["LANGSMITH_PROJECT"] = "default"
+
+if not os.environ.get("GOOGLE_API_KEY"):
+    os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
+
+model = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
+
+messages = [
+    SystemMessage("Translate the following from English into Japanese"),
+    HumanMessage("hi!"),
+]
+
+result = model.invoke(messages)
+print(result)
